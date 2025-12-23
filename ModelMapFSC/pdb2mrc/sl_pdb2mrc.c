b2980edeb6b1c9d7994935d52db39450d28bfc64
gschroe <schroeder.gunnar@gmail.com>
2025-12-23 09:43:48 +0100

    Add GPL licensing information to sl_pdb2mrc.c
    Added licensing information for the tool collection.

--- ModelMapFSC/pdb2mrc/sl_pdb2mrc.c
+++ ModelMapFSC/pdb2mrc/sl_pdb2mrc.c
@@ -21,6 +21,24 @@
  * Date: Apr 1, 2025
  */
 
+/* 
+schroderlab-collection: Tool collection for the processing of Cryo-EM Datasets
+Copyright (C) 2025 Gunnar Schröder
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
