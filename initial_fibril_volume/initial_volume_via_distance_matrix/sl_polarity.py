f05cce916eaf355dd067b1e8c1711af2a90a4697
gschroe <schroeder.gunnar@gmail.com>
2025-12-23 09:51:47 +0100

    Add licensing and usage comments to sl_polarity.py
    Added licensing information and usage instructions.

--- initial_fibril_volume/initial_volume_via_distance_matrix/sl_polarity.py
+++ initial_fibril_volume/initial_volume_via_distance_matrix/sl_polarity.py
@@ -9,6 +9,22 @@ Usage:
   python polarity.py -i particles.star -c class_averages.mrcs -o output_flipped.mrcs
 """
 
+# schroderlab-collection: Tool collection for the processing of Cryo-EM Datasets
+# Copyright (C) 2025 Gunnar Schroeder
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
+
 import argparse
 from pathlib import Path
 import numpy as np
