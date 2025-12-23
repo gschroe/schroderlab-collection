4ad943624e3355a6108bf5d28b6e64eb359f0606
gschroe <schroeder.gunnar@gmail.com>
2025-12-23 09:51:28 +0100

    Add licensing and copyright information
    Added licensing information and copyright notice to the file.

--- initial_fibril_volume/initial_volume_via_distance_matrix/sl_class2d_coord_dist.py
+++ initial_fibril_volume/initial_volume_via_distance_matrix/sl_class2d_coord_dist.py
@@ -13,6 +13,23 @@ Author: Simon Sommerhage
 Date: 2025-12-19
 """
 
+# schroderlab-collection: Tool collection for the processing of Cryo-EM Datasets
+# Copyright (C) 2025 Simon Sommerhage
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
+
+
 import argparse
 import sys
 from pathlib import Path
