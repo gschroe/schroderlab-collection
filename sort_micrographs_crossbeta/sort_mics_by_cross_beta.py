1e99e7f87dc49289d6b883dd2af779d43787c64e
gschroe <schroeder.gunnar@gmail.com>
2025-12-23 09:52:32 +0100

    Add licensing and copyright information
    Added licensing information and copyright notice.

--- sort_micrographs_crossbeta/sort_mics_by_cross_beta.py
+++ sort_micrographs_crossbeta/sort_mics_by_cross_beta.py
@@ -8,6 +8,22 @@ a cross-beta score for each micrograph, corrects for CTF effects, and outputs
 a sorted STAR file.
 """
 
+# schroderlab-collection: Tool collection for the processing of Cryo-EM Datasets
+# Copyright (C) 2025 Simon Sommerhage
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
+
 import argparse
 import sys
 from pathlib import Path
